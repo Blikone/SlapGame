553f294efb1598a692c17c6d19813f97352cf005
Jonathan Steenblik <blikone@hotmail.com>
2016-09-23 16:55:56 -0600

    Fixed chainsaw button

--- game.js
+++ game.js
@@ -1,36 +1,54 @@
 var health = 100;
-var slap = 5;
-var punch = 10;
-var kick = 20;
-var secondTree = false;
+var hatchet = 1;
+var axe = 4;
+var saw = 12;
+var chainsaw = 25;
 
-function onSlap() {
-  health -= slap;
-  update();
-  return health;
+var showChainsaw = function() {
+    var chainsawButton = document.getElementById('chainsaw');
+    var list = chainsawButton.className.split(' ');
+    list.splice(list.indexOf('hidden'), 1);
+    chainsawButton.className = list.join(' ');
 }
-function onPunch() {
-  health -= punch;
-  update();
-  return health;
+
+function Feller() {
+
 }
-function onKick() {
-  health -= kick;
-  update();
-  return health;
+
+function useHatchet() {
+    health -= hatchet;
+    update();
+    return health;
+}
+function useAxe() {
+    health -= axe;
+    update();
+    return health;
+}
+function useSaw() {
+    health -= saw;
+    update();
+    return health;
+}
+function useChainsaw() {
+    health -= chainsaw;
+    update();
+    return health
 }
 function reset() {
-  health = 100;
-  update();
-  secondTree = true;
-  return health;
+    health = 100;
+    update();
+    if (document.getElementById('chainsaw').className.indexOf('hidden') > -1) {
+        showChainsaw();
+    }
+    return health;
 }
 
 function update() {
-  if (health <= 0) {
-    health = 0;
-  }
-  var healthElem = document.getElementById('health');
-  healthElem.innerHTML = health;
+    if (health <= 0) {
+        health = 0;
+    }
+    // var healthElem = document.getElementById('health');
+    // healthElem.innerHTML = health;
 }
 
