f8d0043d55531ddd0603710fdad86aae11d38ccb
Jonathan Steenblik <blikone@hotmail.com>
2016-09-23 15:20:46 -0600

    Game initialized

---
+++ game.js
@@ -0,0 +1,34 @@
+var health = 100;
+var slap = 5;
+var punch = 10;
+var kick = 20;
+
+function onSlap() {
+  health -= slap;
+  update();
+  return health;
+}
+function onPunch() {
+  health -= punch;
+  update();
+  return health;
+}
+function onKick() {
+  health -= kick;
+  update();
+  return health;
+}
+function reset() {
+  health = 100;
+  update();
+  return health;
+}
+
+function update() {
+  if (health <= 0) {
+    health = 0;
+  }
+  var healthElem = document.getElementById('health');
+  healthElem.innerHTML = health;
+}
+
